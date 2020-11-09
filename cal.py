aff581fb85edee5368cd0dd934db653d7566430f
Jiyeon Kim <jiyeon@Jiyeons-MacBook-Pro.local>
2020-11-09 15:37:30 +0900

    first calculator

---
+++ cal.py
@@ -0,0 +1,19 @@
+def add(num1, num2):
+    print (num1 + num2)
+
+add(2, 6)
+
+def substract(num1, num2):
+    print (num1 - num2)
+
+substract(6, 8)
+
+def multiply(num1, num2):
+    print (num1 * num2)
+
+multiply(5, 8)
+
+def divide(num1, num2):
+    print (num1 / num2)
+
+divide(1, 7)
